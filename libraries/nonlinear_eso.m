33f83570d8bdb7a715e84bfb3a4a023643e594c1
mengchaoheng <1215983914@qq.com>
2019-12-01 22:09:03 +0800

    调好ESO，使用TD

--- libraries/nonlinear_eso.m
+++ libraries/nonlinear_eso.m
@@ -1,5 +1,5 @@
 function y=nonlinear_eso(a_i,j,r_i)
-% a_i在(1-1/n_i, i)的区间上，n_i是子系统阶次
+% a_i在(1-1/n_i, i)的区间上，n_i 是子系统阶次
 % y=sign(r_i)*|r_i|^(j*a_i-(j-1))
 y=sign(r_i)*(abs(r_i))^(j*a_i-(j-1));
 end
